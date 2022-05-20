9757318ec5e63be37da22faef27a99c80eefef2f
MikeyBadi <mihaibadia.1999@gmail.com>
2022-05-20 12:35:20 +0200

    chiamata axios fatta

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,56 @@
 <template>
-  <div>
+  <div class="mb-boolflix">
     <HeaderComp/>
-
+    <MainComp/>
   </div>
 </template>
 
 <script>
-import HeaderComp from './components/HeaderComp.vue'
+import HeaderComp from './components/HeaderComp.vue';
+import MainComp from './components/MainComp.vue';
+import axios from 'axios';
 
 export default {
   name: 'App',
   components: {
-    HeaderComp
-  }
+    HeaderComp,
+    MainComp
+  },
+  data() {
+    return {
+      urlAPI:'https://api.themoviedb.org/3/search/movie',
+      apiComp:{
+        api_key:'9d9c00a5c5227c48a18b2cb3044f13c9',
+        language:'it-IT',
+        query: 'avengers'
+      }
+    }
+  },
+  methods: {
+    getAPI(){
+      axios.get(this.urlAPI,{
+        params:this.apiComp
+      })
+      .then(re=>{
+        console.log(re.data.results);
+      })
+      .catch(error =>{
+        console.log(error);
+      })
+    }
+  },
+  mounted(){
+    this.getAPI()
+  },
 }
 </script>
 
 <style lang="scss">
-@import './assets/style/general';
+  @import './assets/style/general';
+
+  .mb-boolflix{
+    height: 100vh;
+    background-color: darkslategrey;
+  }
+
 </style>
---
+++ src/components/MainComp.vue
@@ -0,0 +1,15 @@
+<template>
+  <main>
+    m
+  </main>
+</template>
+
+<script>
+export default {
+  name:'MainComp',
+}
+</script>
+
+<style>
+
+</style>
