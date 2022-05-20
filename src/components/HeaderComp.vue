5a93fffaa485e5a0135b427e45b7a7fe516a6e1a
MikeyBadi <mihaibadia.1999@gmail.com>
2022-05-20 12:17:03 +0200

    header done

---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,21 @@
+<template>
+  <header class=" d-flex justify-content-between align-items-center">
+    <div>
+      <h1>BOOLFIX</h1>
+    </div>
+    <div class="mb-searchFilm">
+    <input type="text" placeholder="Inserisci il titolo del film">
+    <button>Search</button>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComp'
+}
+</script>
+
+<style>
+
+</style>
